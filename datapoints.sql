45582513d9c6099d64aea9273475b1a7c49d8a3f
Azax4 <47921970+Azax4@users.noreply.github.com>
2021-04-05 11:18:19 -0400

    Add files via upload

---
+++ datapoints.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `datapoints` (
+  `xval` int(11) NOT NULL,
+  `yval` int(11) NOT NULL
+);
+INSERT INTO `datapoints` (`xval`, `yval`) VALUES
+(10, 71),
+(10, 71),
+(20, 55),
+(30, 50),
+(40, 65),
+(50, 95),
+(60, 68),
+(70, 28),
+(80, 34),
+(90, 14);
